e1a8af4220a26f1f554f539bfb96231d0df874d3
lintmx <lintmx@gmail.com>
2018-06-06 23:56:33 +0800

    replace npm with yarn

--- hexo/entrypoint.sh
+++ hexo/entrypoint.sh
@@ -3,11 +3,14 @@
 rm -rf source/*
 rm -rf source/.git
 rm -rf source/.gitkeep
+rm -rf source/.gitignore
 rm -rf themes/*
 
-git clone https://github.com/lintmx/blog-archive source/
-git clone https://github.com/lintmx/hexo-theme-tiny themes/tiny
-npm install hexo-render-pug hexo-renderer-stylus --save
+git clone $ARTICLE_REP source/
+git clone $THEME_REP themes/tiny
+
+package=`echo "$THEME_PKG" | awk -F ',' '{for(i=1;i<=NF;i++){printf " "$i}}'`
+yarn add$package
 
 chown 1000:1000 -R source/
 
