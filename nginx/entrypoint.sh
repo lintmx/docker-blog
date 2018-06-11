fdf37af175f4220cb31971f2d645dc2d3d46cb4d
lintmx <lintmx@gmail.com>
2018-06-11 20:33:30 +0800

    add ecc cert support

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -6,12 +6,17 @@ CRON_TASK='1 0 * * * "'$ACME_DIR'"/acme.sh --cron --home "'$ACME_DIR'" > /dev/nu
 issue_domain=`echo "$BLOG_DOMAIN" | awk -F ',' '{for(i=1;i<=NF;i++){printf " -d " $i}}'`
 main_domain=`echo "$BLOG_DOMAIN" | awk -F ',' '{printf $1}'`
 
+if [ ! -z "$ECC_CERT" ]; then
+    ecc_param=' --keylength '$ECC_CERT
+    ecc=' --ecc'
+fi
+
 if [ ! -f "$ACME_DIR/acme.sh" ]; then
     curl https://get.acme.sh | sh
 
-    $ACME_DIR/acme.sh --issue --dns $DNS_PROVIDE$issue_domain
+    $ACME_DIR/acme.sh --issue --dns $DNS_PROVIDE$issue_domain$ecc_param
     
-    $ACME_DIR/acme.sh --issue --dns $DNS_PROVIDE -d $HOOK_DOMAIN
+    $ACME_DIR/acme.sh --issue --dns $DNS_PROVIDE -d $HOOK_DOMAIN$ecc_param
     
 fi
 
@@ -27,14 +32,16 @@ mkdir -p /root/certs/$main_domain
 $ACME_DIR/acme.sh --install-cert -d $main_domain \
                   --key-file        /root/certs/$main_domain/key.pem  \
                   --fullchain-file  /root/certs/$main_domain/cert.pem \
-                  --reloadcmd       "nginx -s reload"
+                  --reloadcmd       "nginx -s reload" \
+                  $ecc
 
 mkdir -p /root/certs/$HOOK_DOMAIN
 
 $ACME_DIR/acme.sh --install-cert -d $HOOK_DOMAIN \
                   --key-file        /root/certs/$HOOK_DOMAIN/key.pem  \
                   --fullchain-file  /root/certs/$HOOK_DOMAIN/cert.pem \
-                  --reloadcmd       "nginx -s reload"
+                  --reloadcmd       "nginx -s reload" \
+                  $ecc
 
 if [ -f "$ACME_DIR/acme.sh" ]; then
     crons=`crontab -l | grep acme`
