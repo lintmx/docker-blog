abf0235d2773e8ad64478bad870425c6cab9b919
lintmx <lintmx@gmail.com>
2018-06-26 23:46:28 +0800

    fix: get theme name error

--- hexo/entrypoint.sh
+++ hexo/entrypoint.sh
@@ -5,12 +5,10 @@ rm -rf source/.git
 rm -rf source/.gitkeep
 rm -rf source/.gitignore
 
-theme_name = `echo -n "$THEME_REP" | sed 's/http.*\/\/.*\/.*\/\(.*\)\.git/\1/g'`
-
 git clone $ARTICLE_REP source/
 
-if [ ! -d "./themes/$theme_name"]; then
-    git clone $THEME_REP themes/$theme_name
+if [ ! -d "./themes/$THEME_NAME" ]; then
+    git clone $THEME_REP themes/$THEME_NAME
 fi
 
 package=`echo "$THEME_PKG" | awk -F ',' '{for(i=1;i<=NF;i++){printf " "$i}}'`
