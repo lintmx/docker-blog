698a4e8230a9d4d78625a42f325f17f8b5a08626
lintmx <lintmx@gmail.com>
2018-08-02 22:41:41 +0800

    support multiple domains

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -3,47 +3,28 @@
 ACME_DIR="/root/.acme.sh"
 CRON_TASK='1 0 * * * "'$ACME_DIR'"/acme.sh --cron --home "'$ACME_DIR'" > /dev/null'
 
-issue_domain=`echo "$BLOG_DOMAIN" | awk -F ',' '{for(i=1;i<=NF;i++){printf " -d " $i}}'`
-main_domain=`echo "$BLOG_DOMAIN" | awk -F ',' '{printf $1}'`
-
-if [ ! -z "$ECC_CERT" ]; then
+if [ ! -z "$ECC_CERT" ]
+then
     ecc_param=' --keylength '$ECC_CERT
     ecc=' --ecc'
 fi
 
-if [ ! -f "$ACME_DIR/acme.sh" ]; then
+for env in $(echo -n "$DNS_API" | awk -F '|' '{for(i=1;i<=NF;i++){print $i}}')
+do
+    export $env
+done
+
+if [ ! -f "$ACME_DIR/acme.sh" ]
+then
     curl https://get.acme.sh | sh
 
-    $ACME_DIR/acme.sh --issue --dns $DNS_PROVIDE$issue_domain$ecc_param
-    
-    $ACME_DIR/acme.sh --issue --dns $DNS_PROVIDE -d $HOOK_DOMAIN$ecc_param
-    
-fi
-
-if [ ! -f "/root/dhparam/dhparam.pem" ]; then
-    mkdir -p /root/dhparam
-    openssl dhparam -out /root/dhparam/dhparam.pem $DHPARAM
-fi
-
-chmod +x $ACME_DIR/acme.sh
-
-mkdir -p /root/certs/$main_domain
-
-$ACME_DIR/acme.sh --install-cert -d $main_domain \
-                  --key-file        /root/certs/$main_domain/key.pem  \
-                  --fullchain-file  /root/certs/$main_domain/cert.pem \
-                  --reloadcmd       "nginx -s reload" \
-                  $ecc
-
-mkdir -p /root/certs/$HOOK_DOMAIN
-
-$ACME_DIR/acme.sh --install-cert -d $HOOK_DOMAIN \
-                  --key-file        /root/certs/$HOOK_DOMAIN/key.pem  \
-                  --fullchain-file  /root/certs/$HOOK_DOMAIN/cert.pem \
-                  --reloadcmd       "nginx -s reload" \
-                  $ecc
-
-if [ -f "$ACME_DIR/acme.sh" ]; then
+    for domain in $(echo -n "$DOMAIN_LIST" | awk -F '|' '{for(i=1;i<=NF;i++){print $i}}')
+    do
+        issue_domain=`echo "$domain" | awk -F ',' '{for(i=1;i<=NF;i++){printf " -d " $i}}'`
+        
+        $ACME_DIR/acme.sh --issue --dns $DNS_PROVIDE$issue_domain$ecc_param
+    done
+else
     crons=`crontab -l | grep acme`
 
     if [ -z "$crons" ]; then
@@ -55,5 +36,25 @@ if [ -f "$ACME_DIR/acme.sh" ]; then
     
     $ACME_DIR/acme.sh --cron --home $ACME_DIR
 fi
+
+if [ ! -f "/root/dhparam/dhparam.pem" -a $DHPARAM -ne 0 ]
+then
+    mkdir -p /root/dhparam
+    openssl dhparam -out /root/dhparam/dhparam.pem $DHPARAM
+fi
+
+source $ACME_DIR/acme.sh.env
+chmod +x $ACME_DIR/acme.sh
+
+for main_domain in $(acme.sh --list | awk '$1 != "Main_Domain" {print $1}')
+do
+    mkdir -p /root/certs/$main_domain
+
+    $ACME_DIR/acme.sh --install-cert -d $main_domain \
+                  --key-file        /root/certs/$main_domain/key.pem  \
+                  --fullchain-file  /root/certs/$main_domain/cert.pem \
+                  --reloadcmd       "nginx -s reload" \
+                  $ecc
+done
 
 exec "$@"
